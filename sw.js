11779aa386b50afafe07169053d1d9c80a5e41aa
Neecocad <42594142+Neecocad@users.noreply.github.com>
2025-10-21 20:28:27 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-
 const CACHE_NAME = 'erosion-offline-v3';
 const ASSETS = [
   './',
@@ -11,9 +10,17 @@ const ASSETS = [
 self.addEventListener('install', (e)=>{
   e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
 });
+
 self.addEventListener('activate', (e)=>{
-  e.waitUntil(caches.keys().then(keys=> Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
 });
+
 self.addEventListener('fetch', (e)=>{
-  e.respondWith(caches.match(e.request).then(res=> res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
